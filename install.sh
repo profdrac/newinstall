d991b0395f3d6f1771a34a9ea8e80b5f5861c893
profdrac <ashu4hire@gmail.com>
2022-06-13 10:44:51 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,10 +12,11 @@ mv scripts .scripts
 chmod +x .scripts/*.sh
 chmod +x .config/polybar/launch.sh
 mkdir ~/Documents/words
+mkdir ~/.config/dunst
 wal -i Pictures/twigs.jpg
 ln -sf ~/.cache/wal/dunstrc ~/.config/dunst/dunstrc
-chmod +x ~/.scripts/Zathura-Pywal/install.sh
-sudo ~/.scripts/Zathura-Pywal/./install.sh
+#chmod +x ~/.scripts/Zathura-Pywal/install.sh
+#sudo ~/.scripts/Zathura-Pywal/./install.sh
 sudo pacman -S virtualbox-guest-utils
 sudo systemctl enable vboxservice
 sudo usermod -a -G vboxsf ashu
